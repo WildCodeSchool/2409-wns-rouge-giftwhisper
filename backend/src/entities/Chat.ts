87bfa587391ac8c34184e5656393d391e4760672
PRISCILLA PAYET <priscilla.ppayet@gmail.com>
2025-03-12 15:16:25 +0100

    ✨ feat: Complete user CRUD Implemented and tested `getUserById`, `getUsers`, `updateUser`, and `deleteUser`

--- backend/src/entities/Chat.ts
+++ backend/src/entities/Chat.ts
@@ -1,8 +1,17 @@
-import { Field } from "type-graphql";
-import { Column, CreateDateColumn, Entity, JoinTable, ManyToMany, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
+import { Field, ObjectType } from "type-graphql";
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  JoinTable,
+  ManyToMany,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from "typeorm";
 import { User } from "./User";
 
 @Entity()
+@ObjectType()
 export class Chat {
   @PrimaryGeneratedColumn()
   id!: number;
@@ -11,14 +20,14 @@ export class Chat {
   @Field()
   name!: string;
 
-  @CreateDateColumn({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP' })
+  @CreateDateColumn({ type: "timestamp", default: () => "CURRENT_TIMESTAMP" })
   @Field()
   createdAt!: Date;
 
-  @UpdateDateColumn({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP' })
+  @UpdateDateColumn({ type: "timestamp", default: () => "CURRENT_TIMESTAMP" })
   @Field()
   updatedAt!: Date;
-  
+
   @ManyToMany(() => User, (user) => user.chats)
   @JoinTable()
   @Field(() => [User])
--- backend/src/entities/User.ts
+++ backend/src/entities/User.ts
@@ -1,12 +1,21 @@
-import { Field, ID, ObjectType } from "type-graphql";
-import { BaseEntity, Column, CreateDateColumn, Entity, ManyToMany, OneToMany, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
+import { Field, ID, InputType, ObjectType } from "type-graphql";
+import {
+  BaseEntity,
+  Column,
+  CreateDateColumn,
+  Entity,
+  ManyToMany,
+  OneToMany,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from "typeorm";
 import { Chat } from "./Chat";
 import { UserGroup } from "./UserGroup";
+import { IsEmail, IsStrongPassword } from "class-validator";
 
 @Entity()
 @ObjectType()
 export class User extends BaseEntity {
-  
   @PrimaryGeneratedColumn()
   @Field(() => ID)
   id!: number;
@@ -34,17 +43,17 @@ export class User extends BaseEntity {
   @Field()
   is_verified!: boolean;
 
-  @UpdateDateColumn({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP' })
+  @UpdateDateColumn({ type: "timestamp", default: () => "CURRENT_TIMESTAMP" })
   @Field()
   last_login!: Date;
 
-  @CreateDateColumn({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP' })
+  @CreateDateColumn({ type: "timestamp", default: () => "CURRENT_TIMESTAMP" })
   @Field()
   created_at!: Date;
 
-  @UpdateDateColumn({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP' })
+  @UpdateDateColumn({ type: "timestamp", default: () => "CURRENT_TIMESTAMP" })
   @Field()
-  updated_at!: Date;  
+  updated_at!: Date;
 
   @ManyToMany(() => Chat, (chat) => chat.users)
   @Field(() => [Chat])
@@ -53,4 +62,51 @@ export class User extends BaseEntity {
   @OneToMany(() => UserGroup, (userGroup) => userGroup.user)
   @Field(() => [UserGroup])
   userGroups!: UserGroup[];
+}
+
+@InputType()
+export class UserCreateInput {
+  @IsEmail()
+  @Field()
+  email!: string;
+
+  @Field()
+  @IsStrongPassword()
+  password!: string;
+
+  @Field()
+  first_name!: string;
+
+  @Field()
+  last_name!: string;
+
+  @Field()
+  date_of_birth!: Date;
+
+  @Field()
+  is_verified!: boolean;
+}
+
+@InputType()
+export class UserUpdateInput {
+  @Field({ nullable: true })
+  email?: string;
+
+  @Field({ nullable: true })
+  password?: string;
+
+  @Field({ nullable: true })
+  first_name?: string;
+
+  @Field({ nullable: true })
+  last_name?: string;
+
+  @Field({ nullable: true })
+  date_of_birth?: Date;
+
+  @Field({ nullable: true })
+  is_verified?: boolean;
+
+  @Field({ nullable: true })
+  last_login?: Date;
 }
