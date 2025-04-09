0c93530f12eaf9eb5e3d755a23c3efd4a4f4a436
jeremy <jeremy.marzuolo@gmail.com>
2025-04-09 09:30:58 +0200

    chat window work in progress

--- frontend/src/components/Layout.tsx
+++ frontend/src/components/Layout.tsx
@@ -1,9 +1,11 @@
-import { Outlet, Link } from "react-router-dom";
+import { Outlet, Link, useLocation } from "react-router-dom";
 
 export function PageLayout() {
+  const location = useLocation();
   return (
-    <body>
-      <main>
+    <main>
+      {location.pathname !== '/chat-window'
+        ?
         <ul>
           <li><Link to={'/'}>Home</Link></li>
           <li><Link to={'/sign-in'}>Sign In</Link></li>
@@ -17,8 +19,9 @@ export function PageLayout() {
           <li><Link to={'/group-settings'}>Group Settings</Link></li>
           <li><Link to={'/chat-window'}>Chat Windows</Link></li>
         </ul>
-        <Outlet />
-      </main>
-    </body>
+        : null
+      }
+      <Outlet />
+    </main>
   );
 }
--- frontend/src/pages/ChatWindow.tsx
+++ frontend/src/pages/ChatWindow.tsx
@@ -1,6 +1,51 @@
+import { FormEvent, useState } from "react";
+
+const baseMessages = [
+  { id: 1, author: 'me', text: 'Hi, our first message on this awesome app :) !' },
+  { id: 2, author: 'jean-claude', text: "Pourquoi qu'il parle en anglais lui" },
+  { id: 3, author: 'me', text: 'Oooooh, sacré Jean-Claude haha' },
+  { id: 4, author: 'murielle', text: "Est ce que c'est ça google ?" },
+  { id: 5, author: 'murielle', text: "recette tarte citron meringuée" },
+  { id: 6, author: 'me', text: "Seigneur, à l'aide" },
+  { id: 7, author: 'jean-claude', text: "Apéro !" },
+];
+
 function ChatWindow() {
-  return(
-    <h1>I'm the ChatWindow !</h1>
+  const giftReceiver = 'Fabrice';
+  const [message, setMessage] = useState('');
+  const [messages, setMessages] = useState(baseMessages);
+
+  const submit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (!message.length) return;
+    setMessages((e) => {
+      const messagesCopy = structuredClone(e);
+      const nextId = e[e.length - 1].id + 1;
+      messagesCopy.push({ id: nextId, author: 'me', text: message });
+      return messagesCopy;
+    });
+    setMessage('');
+  };
+
+  return (
+    <>
+      <article className="chat-window">
+        <h1>⬅️ Pour {giftReceiver}</h1>
+        <section className="messages">
+          {messages.map((message) => {
+            return (
+              <div key={message.id} className={`message${message.author !== 'me' ? ' align-right' : ''}`}>
+                <p>{message.author} : {message.text}</p>
+              </div>
+            )
+          })}
+        </section>
+        <form className="message-form" onSubmit={submit}>
+          <input value={message} onChange={(e) => setMessage(e.currentTarget.value)} name="chat-message" id="chat-message" type="text" />
+          <button type="submit">➡️</button>
+        </form>
+      </article>
+    </>
   )
 }
 
