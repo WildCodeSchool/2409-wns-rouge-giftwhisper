da643238e10041c4c0ba098d3b1158dbb86120d7
jeremy <jeremy.marzuolo@gmail.com>
2025-04-10 10:31:49 +0200

    chat window first complete draft

--- frontend/src/components/PageLayout.tsx
+++ frontend/src/components/PageLayout.tsx
@@ -5,13 +5,12 @@ import { HeaderContent } from "./HeaderContent";
 
 export function PageLayout() {
 
-  
   return (
     <>
-      <HeaderPage classname="bg-[#D36567] flex items-center h-[64px] md:h-[120px] px-5 py-2 md:px-11 md:py-8">
-      <HeaderContent />
-      </HeaderPage>
-      <main>
+      <main className="h-screen flex flex-col">
+        <HeaderPage classname="bg-[#D36567] flex items-center h-[64px] md:h-[120px] px-5 py-2 md:px-11 md:py-8">
+          <HeaderContent />
+        </HeaderPage>
         <Outlet />
       </main>
     </>
