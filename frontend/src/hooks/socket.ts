be41de6cb4e1aa8373f1eebdb2ff62277ee5b06d
jeremy <jeremy.marzuolo@gmail.com>
2025-05-14 15:34:19 +0200

    remove fixed todo

--- frontend/src/hooks/socket.ts
+++ frontend/src/hooks/socket.ts
@@ -2,7 +2,6 @@ import { useRef } from "react";
 import { Socket } from "socket.io-client";
 import { io } from "socket.io-client";
 
-//TODO : fix issues with socket path since the vite proxy is not in use anymore
 export function useSocket() {
   const socketRef = useRef<Socket>(null);
   const disconnectSocket = () => {
