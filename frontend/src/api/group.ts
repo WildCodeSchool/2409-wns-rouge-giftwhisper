937dc1856ec8b916991edfd762a1850ae0dbc24f
isil-isil <isil.isil.pro@gmail.com>
2025-06-11 11:30:08 +0200

    feat(groupe): Connect creation form to backend

---
+++ frontend/src/api/group.ts
@@ -0,0 +1,59 @@
+import { gql } from "@apollo/client";
+
+export const GET_GROUP = gql`
+  query GetGroup($id: ID!) {
+    group(id: $id) {
+      id
+      name
+      end_date
+      is_secret_santa
+      is_active
+      users {
+        id
+        first_name
+        last_name
+        email
+      }
+    }
+  }
+`;
+
+export const CREATE_GROUP = gql`
+  mutation CreateGroup($data: GroupCreateInput!) {
+    createGroup(data: $data) {
+        id
+        name
+        end_date
+        is_secret_santa
+        created_at
+    }
+  }
+`;
+
+export const ADD_USERS_TO_GROUP = gql`
+  mutation AddUsersToGroupByEmail($emails: [String!]!, $groupId: ID!) {
+    addUsersToGroupByEmail(emails: $emails, groupId: $groupId) {
+      id
+      name
+      users {
+        id
+        email
+      }
+    }
+  }
+`;
+
+export const REMOVE_USER_FROM_GROUP = gql`
+  mutation RemoveUserFromGroup($groupId: ID!, $userId: ID!) {
+    removeUserFromGroup(groupId: $groupId, userId: $userId) {
+      id
+      name
+      users {
+        id
+        first_name
+        last_name
+        email
+      }
+    }
+  }
+`;
